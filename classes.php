64cd43064781143606c688ed58587074d5991dbd
Harlley Oliveira <harlley@gmail.com>
2017-01-01 20:06:38 +0000

    CanalYoutube private methods is done

--- classes.php
+++ classes.php
@@ -11,18 +11,38 @@ class Video {
 }
 
 class CanalYoutube {
+  
+  private function getIdVideosFromAPI() {
+    $url = "https://api.myjson.com/bins/ec9qf";
+    $json = file_get_contents($url);
+    $data = json_decode($json);
+    return array_map(function($item) { return $item->id->videoId; }, $data->items);
+  }
+  
+  
+  private function getImageFromAPI($videoId) {
+    $url = "https://api.myjson.com/bins/1e4f6v";
+    $json = file_get_contents($url);
+    $data = json_decode($json);
+    return $data->items[0]->snippet->thumbnails->medium->url;
+  }
+  
+  
   public static function getVideos() {
-    $video1 = new Video("image1", "link1");
-    $video2 = new Video("image2", "link2");
-    $video3 = new Video("image3", "link3");
+    //return self::getIdVideosFromAPI();
     
-    return array($video1, $video2, $video3);
+    return self::getImageFromAPI("teste");
+    // $video1 = new Video("image1", "link1");
+    // $video2 = new Video("image2", "link2");
+    // $video3 = new Video("image3", "link3");
+    
+    // return array($video1, $video2, $video3);
 
   }
 }
 
 echo "<pre>";
-print_r(CanalYoutube::getVideos());
+echo CanalYoutube::getVideos();
 echo "</pre>";
 
 
